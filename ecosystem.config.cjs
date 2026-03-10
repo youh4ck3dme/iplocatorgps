6b595f34f31ca4fd10d239c18e5fe02e2bf8ffca
you4ck3dme <h4ck3d@h4ck3d.me>
2026-03-10 23:14:50 +0100

    fix: use dynamic port in pm2 config to prevent rollback

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'iplocatorgps',
       cwd: '/var/www/iplocatorgps',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 9999',
+      args: 'start -p ' + (process.env.PORT || '9999'),
       env: {
         NODE_ENV: 'production'
       }
